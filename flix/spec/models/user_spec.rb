39238b9ac1eeba3d13cab4134297ece08b2633e8
Brandon Braun <brandon.braun@gmail.com>
2015-07-24 15:13:52 -0400

    Completed section 4 part 2, 25% done

--- flix/spec/models/user_spec.rb
+++ flix/spec/models/user_spec.rb
@@ -85,3 +85,20 @@ describe "A user" do
 	end
 end
 
+describe "authenticate" do
+	before do
+		@user = User.create!(user_attributes)
+	end
+
+	it "returns non-ture value if the email does not match" do
+		expect(User.authenticate("nomatch", @user.password)).not_to eq(true)
+	end
+
+	it "returns non-true value if the password does not mathc" do
+		expect(User.authenticate(@user.email, "nomatch")).not_to eq(true)
+	end
+
+	it "returns the user if the email and password match" do
+		expect(User.authenticate(@user.email, @user.password)).to eq(@user)
+	end
+end
