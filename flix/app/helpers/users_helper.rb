248f8082128ea696333966ea6ad63ecfae88f063
Brandon Braun <brandon.braun@gmail.com>
2015-07-25 10:04:26 -0400

    Completed section 8, 61 percent done

--- flix/app/helpers/users_helper.rb
+++ flix/app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
-	def profile_image_for(user)
-		image_tag "http://secure.gravatar.com/avatar/#{user.gravatar_id}", alt: user.name
+	def profile_image_for(user, options={})
+		size = options[:size] || 80
+		image_tag "http://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}", alt: user.name
 	end
 end
